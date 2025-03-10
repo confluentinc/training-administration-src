4a2dc51b97c826a6c787e806cfd69ac109029cff
Antonio Loma Daza <48225958+confluent-antonio@users.noreply.github.com>
2025-03-10 19:38:03 +0100

    Re-write of the script
    * Shell safety options added
    * Ran script by ShellCheck
    * Test for missing/broken files
    * Idempotent script
    * Updated for `compose.yaml` re-naming

--- update-environment.sh
+++ update-environment.sh
@@ -15,15 +15,70 @@
 # limitations under the License.
 #
 
-cat <<EOF >~/.myvars
-export CLUSTERID=$(grep -m1 CLUSTER_ID: ~/confluent-admin/docker-compose.yml | tr -d \ | cut -d: -f2)
-export CONTROLLERS="9991@controller-1:19093,\ 9992@controller-2:29093,9993@controller-3:39093"
+# Script safety options
+set -o errexit
+set -o nounset
+# Set pipefail if it works in a subshell, disregard if unsupported
+# shellcheck disable=SC3040
+(set -o pipefail 2> /dev/null) && set -o pipefail
+
+# Set up some local variables
+VARFILE="$HOME/.cpvars" # Where we're writing CP- & Kafka-related values
+# shellcheck disable=SC2088
+PRINTVARFILE="~/$(basename "$VARFILE")"
+BRC="$HOME/.bashrc"
+DOCKERFILE="$HOME/confluent-admin/compose.yaml"
+
+# Function to get a parameter from the environment of the Docker compose file
+get_env_param() {
+    grep -m1 "$2": "$1" 2>/dev/null | sed 's/^[^:]*: //';
+}
+
+# Find CLUSTER_ID or bail on Docker compose file errors
+CLUSTERID=$(get_env_param "$DOCKERFILE" 'CLUSTER_ID')
+if [ -z "$CLUSTERID" ] ; then
+  echo "Error: $DOCKERFILE doesn't exist or it doesn't define CLUSTER_ID. Exiting"
+  exit 1
+fi
+
+# Find CONTROLLER_QUORUM_VOTERS or bail on Docker compose file errors
+CONTROLLERS=$(get_env_param "$DOCKERFILE" 'KAFKA_CONTROLLER_QUORUM_VOTERS')
+if [ -z "$CONTROLLERS" ] ; then
+  echo "Error: $DOCKERFILE doesn't exist or it doesn't define KAFKA_CONTROLLER_QUORUM_VOTERS. Exiting"
+  exit 1
+fi
+
+# Create and populate the file with relevant CP variables
+if [ ! -f "$VARFILE" ] ; then
+cat <<EOF >"$VARFILE"
+export CLUSTERID=$CLUSTERID
+export CONTROLLERS="$CONTROLLERS"
 export BOOTSTRAPS="kafka-1:19092,kafka-2:29092,kafka-3:39092"
 EOF
+fi
 
-x='source ~/.myvars' ; grep -qxF "$x" ~/.bashrc || echo "$x" >>~/.bashrc
+if [ -f "$BRC" ] ; then
+    # Make the shell (assumed to be Bash) to source our variables on start-up
+    # First, test if it's already been done
+    if grep -qxF "source $VARFILE" "$BRC" ; then
+        echo "Startup already set-up. To add the variables to this terminal, please run:"
+        echo "source $PRINTVARFILE"
+        echo ""
+        exit 0
+    fi
+else
+   # No Bash, no luck
+   echo "No .bashrc found for current user. If login shell has been set up differently, please configure the corresponding login file and/or source this file in all your shells:"
+   echo "source $PRINTVARFILE"
+   echo ""
+   exit 0
+fi
+
+# Really do make bash source our variables on start-up
+echo "source $PRINTVARFILE" >>"$BRC"
 cat <<EOF
-To add the variables to a terminal that is already opened (like this one), please run ". ~/.bashrc"
+To add the variables to a terminal that is already opened (like this one), please run:
+source $PRINTVARFILE
 For terminals opened from this point onwards, the variables will be automatically added.
-EOF
 
+EOF
