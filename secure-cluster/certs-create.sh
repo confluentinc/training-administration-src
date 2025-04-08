31c6457df56d73067d22168cf74465d52a732c8b
Antonio Loma Daza <48225958+confluent-antonio@users.noreply.github.com>
2025-04-08 11:45:54 +0200

    7.8.0 v1.0.1 draft (#25)
    * Added script to update environment
    
    * Improved instructions about tools container
    
    * Also, fixed some typos
    
    * Added LICENSE file as per Company's policy
    
    * Minor changes
    
    * Also, added copyright & license header
    
    * Updated host entry list with controllers
    
    * Changed idempotence test to be course-independent
    * Also, added copyright & license header
    
    * Added script to define useful env vars in bashrc
    
    Previously, students were instructed to define them from shell but some had problems with copy&paste.
    Also, renamed to fix typo in file name
    
    * Added project files as per IDEA's recommendation
    
    * Updated cp-server container version to 7.8.1
    
    * Updated gitignore and remove ignor-able files
    
    * Updated (and fixed typo in name) script about env vars
    
    * Fixed inter-broker crashing new containers
    
    * A new test on listeners, inter-broker listeners and controller listeners (including default values) could cause a boot-up error regarding wrong configuration
    * Also, disabled SBC (enabled by default in our docker images but not officially in properties documentation)
    * Also, removed obsolete commented config.
    
    * Updated README.md and deleted obsolete files
    
    * Re-write of the script
    
    * Shell safety options added
    * Ran script by ShellCheck
    * Test for missing/broken files
    * Idempotent script
    * Updated for `compose.yaml` re-naming
    
    * Various changes container composition
    
    * Moved to `docker compose` (aka compose V2)
    * [compose V2] Renamed `docker-compose.yml` to `compose.yaml`
    * [compose V2] Removed YAML start-of-document and `version`
    * [compose V2] Enclosed `ports` definitions in double quotes
    * Added health check for controllers based on `kafka-metadata-quorum` (that should be installed in cp-server)
    * Added configuration for NG Consumer Group Protocol (commented out)
    * Changed health check to one based on `kafka-topics` (that should be installed in cp-server) for faster composition
    * Brokers now depend on controllers being *healthy*
    * Added production-recommended dependencies: Connect on SR, CCC on both Connect and SR (commented out for faster lab boot-up)
    * `README.md` updated bootstraps for current deployment
    
    * Small tweaks
    
    * Changed long license comment to SPDX format
    * Tweaked Docker healthchecks to balance performance
    * Added data/ directory so it's not owned by superuser
    
    * Reimplemented Security Lab scripts
    
    * Create *-creds directories through hidden files for easier clean-up
    * Added Copyright & License information for script and compose files.
    * Renamed docker-compose.yml to compose.yaml (Compose V2)
    * Enclosed ports ranges in quotes (Compose V2)
    * compose.yaml: transpose changes from main Compose file regarding changes in listener configuration.
    * compose.yaml: transpose healthcheck tests and dependencies
    * compose.yaml: changed *command* in brokers to reflect changes in certs-create.sh
    * compose.yaml: moved security-related configuration for brokers at the top of *environment* so students can find them more easily
    * certs-create.sh: complete rewrite: script valid for generic *sh* (ShellCheck), safer cleanup, removed redundant generation of truststores, removed creation of extra files for unimplemented SR-related lab, and used variables to improve readability.
    
    * Pre-publishing modification

--- secure-cluster/certs-create.sh
+++ secure-cluster/certs-create.sh
@@ -1,62 +1,71 @@
-#!/bin/bash
+#! /bin/sh
+#
+# SPDX-FileCopyrightText: Copyright (c) 2019-2025 Confluent Inc.
+# SPDX-License-Identifier: Apache-2.0
+#
 
-set -o nounset \
-    -o errexit \
-    -o verbose
-#    -o xtrace
+# Script safety options
+set -o errexit
+set -o nounset
+# Set pipefail if it works in a subshell, disregard if unsupported
+# shellcheck disable=SC3040
+(set -o pipefail 2> /dev/null) && set -o pipefail
+
+# Set up some local variables
+CONF_DIR="$HOME/confluent-admin/secure-cluster"
+TRUSTSTORE="$CONF_DIR/truststore.jks"
+CRED="confluent"
 
 # Cleanup files
-find . \( -type f -name "*.crt" -o -name "*.csr" -o -name "*_creds" -o -name "*.jks" -o -name "*.srl" -o -name "*.key" -o -name "*.pem" -o -name "*.der" -o -name "*.p12" \)  -delete
+rm -f "$CONF_DIR"/*-creds/* "$CONF_DIR"/ca.??? "$CONF_DIR"/truststore.jks
 
 # Generate CA key
-openssl req -new -x509 -keyout ca.key -out ca.crt -days 365 -subj '/CN=ca1.test.confluent.io/OU=TEST/O=CONFLUENT/L=PaloAlto/S=Ca/C=US' -passin pass:confluent -passout pass:confluent
+openssl req -new -x509 -keyout ca.key -out ca.crt -days 365 -subj '/CN=ca1.test.confluent.io/OU=TEST/O=CONFLUENT/L=MountainView/ST=CA/C=US' -passin pass:confluent -passout pass:confluent
 
+# Create a truststore with the CA
+keytool -noprompt -keystore "$TRUSTSTORE" -alias CARoot -import -file ca.crt -storepass ${CRED}
 
 for i in kafka-1 kafka-2 kafka-3 client
 do
 	echo "------------------------------- $i -------------------------------"
+	WORKDIR="$CONF_DIR/$i-creds"
+	KEYSTORE="$WORKDIR/kafka.$i.keystore.jks"
+	CS_REQUEST="$WORKDIR/$i.csr"
+	CERT_FILE="$WORKDIR/$i-ca1-signed.crt"
 
 	# Create host keystore
 	keytool -genkey -noprompt \
-				 -alias $i \
-				 -dname "CN=$i,OU=TEST,O=CONFLUENT,L=PaloAlto,S=Ca,C=US" \
-                                 -ext san=dns:$i \
-				 -keystore $i-creds/kafka.$i.keystore.jks \
-				 -keyalg RSA \
-				 -storepass confluent \
-				 -keypass confluent
+    -alias "$i" \
+    -dname "CN=$i,OU=TEST,O=CONFLUENT,L=MountainView,ST=CA,C=US" \
+    -ext "san=dns:$i" \
+    -keystore "$KEYSTORE" \
+    -keyalg RSA \
+    -storepass ${CRED} \
+    -keypass ${CRED}
 
 	# Create the certificate signing request (CSR)
-	keytool -keystore $i-creds/kafka.$i.keystore.jks -alias $i -certreq -file $i-creds/$i.csr -storepass confluent -keypass confluent
+	keytool -keystore "$KEYSTORE" -alias $i -certreq -file "$CS_REQUEST" -storepass ${CRED} -keypass ${CRED}
 
-        # Sign the host certificate with the certificate authority (CA)
-	openssl x509 -req -CA ca.crt -CAkey ca.key -in $i-creds/$i.csr -out $i-creds/$i-ca1-signed.crt -days 9999 -CAcreateserial -passin pass:confluent
+	# Sign the host certificate with the certificate authority (CA)
+	openssl x509 -req -CA ca.crt -CAkey ca.key -in "$CS_REQUEST" -out "$CERT_FILE" -days 9999 -CAcreateserial -passin pass:${CRED}
 
-        # Sign and import the CA cert into the keystore
-	keytool -noprompt -keystore $i-creds/kafka.$i.keystore.jks -alias CARoot -import -file ca.crt -storepass confluent -keypass confluent
+	# Sign and import the CA cert into the keystore
+	keytool -noprompt -keystore "$KEYSTORE" -alias CARoot -import -file ca.crt -storepass ${CRED}
+	# -keypass ${CRED}
 
-        # Sign and import the host certificate into the keystore
-	keytool -noprompt -keystore $i-creds/kafka.$i.keystore.jks -alias $i -import -file $i-creds/$i-ca1-signed.crt -storepass confluent -keypass confluent
+	# Sign and import the host certificate into the keystore
+	keytool -noprompt -keystore "$KEYSTORE" -alias $i -import -file "$CERT_FILE" -storepass ${CRED} -keypass ${CRED}
 
-	# Create truststore and import the CA cert
-	keytool -noprompt -keystore $i-creds/kafka.$i.truststore.jks -alias CARoot -import -file ca.crt -storepass confluent -keypass confluent
+	# Copy the truststore inside so it will be visible inside the Broker's container
+	cp "$TRUSTSTORE" "$WORKDIR/kafka.$i.truststore.jks"
 
-	# Save creds
-  	echo "confluent" > ${i}-creds/${i}_sslkey_creds
-  	echo "confluent" > ${i}-creds/${i}_keystore_creds
-  	echo "confluent" > ${i}-creds/${i}_truststore_creds
-
-	# Create pem files and keys used for Schema Registry HTTPS testing
-	#   openssl x509 -noout -modulus -in client.certificate.pem | openssl md5
-	#   openssl rsa -noout -modulus -in client.key | openssl md5 
-        #   echo "GET /" | openssl s_client -connect localhost:8082/subjects -cert client.certificate.pem -key client.key -tls1_2 
-	keytool -export -alias $i -file $i-creds/$i.der -keystore $i-creds/kafka.$i.keystore.jks -storepass confluent
-	openssl x509 -inform der -in $i-creds/$i.der -out $i-creds/$i.certificate.pem
-	keytool -importkeystore -srckeystore $i-creds/kafka.$i.keystore.jks -destkeystore $i-creds/$i.keystore.p12 -deststoretype PKCS12 -deststorepass confluent -srcstorepass confluent -noprompt
-	openssl pkcs12 -in $i-creds/$i.keystore.p12 -nodes -nocerts -out $i-creds/$i.key -passin pass:confluent
+	# Save JKS credentials
+	echo ${CRED} > "$WORKDIR/${i}_sslkey_creds"
+	ln "$WORKDIR/${i}_sslkey_creds" "$WORKDIR/${i}_keystore_creds"
+	ln "$WORKDIR/${i}_sslkey_creds" "$WORKDIR/${i}_truststore_creds"
 
 done
 
-# waiting for brokers to become available
-		
-while ! openssl s_client -connect kafka-1:19093 -tls1_2 > /dev/null 2>&1 <<< "q" ; do echo -n "." & sleep 1 ; done
+echo "Waiting for brokers to become available ..."
+while ! echo "q" | openssl s_client -connect kafka-1:19093 -tls1_3 > /dev/null 2>&1 ; do sleep 2 ; done
+echo "Brokers ready"
